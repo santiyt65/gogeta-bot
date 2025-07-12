5d2de8a32d58f00689e8e9923e512698a474eafa
L.N. <santiloez6@gmail.com>
2025-07-12 00:33:15 -0300

    GOGETA - BOT

--- comandos/index.js
+++ comandos/index.js
@@ -7,7 +7,7 @@ export { comandoMute } from './admin/mute.js';
 export { comandoUnmute } from './admin/unmute.js';
 export { comandoListaMute } from './admin/listamute.js';
 export { comandoResetRanking } from './admin/resetRanking.js';
-export { comandomoderacionpreguntas, manejarBotonRespuesta } from './admin/moderacionpreguntas.js';
+export { comandoModeracionPreguntas, manejarBotonRespuesta } from './admin/moderacionpreguntas.js';
 export { comandoEstadoPregunta } from './admin/estadopregunta.js';
 
 // Juegos
